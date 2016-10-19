6f776b073241b9dde08d5fce3cfdba0908428d5b
krashaen <kmgqq@mail.ru>
2016-10-19 15:38:51 +0700

    refactoring show_comments

--- app/post-comments/show_comments.js
+++ app/post-comments/show_comments.js
@@ -1,3 +1,36 @@
+function createCommentBlock(id) {
+    const comment = document.createElement('div');
+    comment.className = 'post-comments';
+    comment.setAttribute('id', id);
+    return comment;
+}
+
+function createCommentTitle(comment) {
+    const commentTitle = document.createElement('h2');
+    commentTitle.className = 'post-comments__comment-name';
+    commentTitle.textContent = comment.name;
+    return commentTitle;
+}
+
+function createCommentBody(comment) {
+    const commentBody = document.createElement('p');
+    commentBody.className = 'post-comments__comment-content';
+    commentBody.textContent = comment;
+    return commentBody;
+}
+
+function createUserEmail() {
+    const commentUserEmail = document.createElement('div');
+    commentUserEmail.className = 'post-comments__user-email';
+    return commentUserEmail;
+}
+
+function createUserAddress(email) {
+    const userEmail = document.createElement('address');
+    userEmail.textContent = email;
+    return userEmail;
+}
+
 function showCommentList(id, comments) {
     const commentBtn = document.getElementsByClassName('posts-list__send');
     const postBlock = document.getElementsByClassName('posts-list__post');
@@ -6,27 +39,21 @@ function showCommentList(id, comments) {
         for (let i = 0; i < postBlock.length; i += 1) {
             const postBlockId = parseInt(postBlock[i].getAttribute('id'), 10);
             if (postBlockId === id) {
-                const comment = document.createElement('div');
-                comment.className = 'post-comments';
-                comment.setAttribute('id', id);
+                const comment = createCommentBlock(id);
                 postBlock[i].insertBefore(comment, postBlock[i].children[3]);
 
-                const commentTitle = document.createElement('h2');
-                commentTitle.className = 'post-comments__comment-name';
-                commentTitle.textContent = entry.name;
+                const commentTitle = createCommentTitle(entry.name);
                 comment.appendChild(commentTitle);
 
-                const commentBody = document.createElement('p');
-                commentBody.className = 'post-comments__comment-content';
-                commentBody.textContent = entry.body;
+                const commentBody = createCommentBody(entry.body);
                 comment.appendChild(commentBody);
 
-                const commentUserEmail = document.createElement('div');
-                commentUserEmail.className = 'post-comments__user-email';
+                const commentUserEmail = createUserEmail();
                 comment.appendChild(commentUserEmail);
-                const UserEmail = document.createElement('address');
-                UserEmail.textContent = entry.email;
-                commentUserEmail.appendChild(UserEmail);
+
+                const userEmail = createUserAddress(entry.email);
+                commentUserEmail.appendChild(userEmail);
+
                 commentBtn[i].textContent = 'Закрыть';
                 commentBtn[i].setAttribute('href', `#${id}`);
                 commentBtn[i].onclick = view.closeComment.bind(null, id, comments);
